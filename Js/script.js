72b75469e05bdf4b9725ea806e2e1867e3119565
Mateusz Grincewicz <matek160393@gmail.com>
2021-11-21 20:07:02 +0100

    modification of functions JS

--- Js/script.js
+++ Js/script.js
@@ -1,17 +1,20 @@
-console.log("Cześć")
-let hideQuotes = document.querySelector(".js-buttonHideQuotes");
-let favoriteQuotes = document.querySelector(".section__js-favoriteQuotes");
-
-
-hideQuotes.addEventListener("click", () => {
-    favoriteQuotes.classList.toggle("section__js-showQuotes");
-    if (favoriteQuotes.classList.contains("section__js-showQuotes")) {
-        hideQuotes.innerText = "Pokaż cytaty";
+{
+    const welcome = () => {
+        console.log("Cześć!")
     }
-    else {
-        hideQuotes.innerText = "Ukryj cytaty";
+    welcome();
 
+    let hideQuotes = document.querySelector(".js-buttonHideQuotes");
+    const toggleClassQuotes = () => {
+        const favoriteQuotes = document.querySelector(".section__js-favoriteQuotes");
+        favoriteQuotes.classList.toggle("section__js-showQuotes");
+        hideQuotes.innerText = favoriteQuotes.classList.contains("section__js-showQuotes") ? "Pokaż cytaty" : "Ukryj cytaty";
     }
-});
+    
+    const init = () => {
+        hideQuotes.addEventListener("click", toggleClassQuotes);
+    }
+    init();
+}
 
 
